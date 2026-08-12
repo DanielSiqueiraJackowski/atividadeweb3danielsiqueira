662cbcbcba84455f7a25bd5ee0afa5054d0de372
ArturSimoni <artursimonijesus@gmail.com>
2026-08-12 11:16:58 -0300

    att2

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,21 @@ app.use(express.json())
 
 app.get('/api/mensagem', (req, res) => {
     res.json({ texto: "Olá do Servidor!"})
+});
+
+app.get('/cep/:cep', async (req, res) => {
+    const { cep } = req.params
+
+    try {
+        const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const dados = await resposta.json();
+
+        if (dados.erro) return res.status(404).json({ erro: 'CEP não encontrado' });
+
+        res.status(200).json(dados);
+    } catch (error) {
+        res.status(500).json({ erro: 'Erro ao buscar o CEP' })
+    }
 })
 
 app.listen(3000)
